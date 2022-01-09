5c2459bbea667ad866b01f8de9d936694054d548
whosshazo <ryandeshazo1@gmail.com>
2022-01-09 11:56:46 -0500

    initial push

---
+++ develop/script1.js
@@ -0,0 +1,45 @@
+$(document).ready(
+    function () {
+        $(".saveBtn").on("click", function() {
+            var value = $(this).siblings(".description").val()
+            var time = $(this).parent().attr("id")
+            
+            localStorage.setItem(time, value);
+        })
+        function updatingHours() {
+            var currentMoment = moment().hour();
+            $(".time-block").each(function () {
+                var hourBlock = parseInt($(this).attr("id").split("-")[1])
+                if (hourBlock < currentMoment) {
+                    $(this).addClass("past");
+                    
+                } else if (hourBlock === currentMoment) {
+                    $(this).removeClass("past");
+                    $(this).addClass("present");
+                } else  {
+                    $(this).removeClass("past");
+                    $(this).removeClass("present");
+                    $(this).addClass("future");
+                }
+            });
+        }
+        updatingHours();
+        
+        var checkingHour = setInterval(updatingHours, 10000);
+        
+        $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+        $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+        $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+        $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+        $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+        $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+        $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+        $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+        $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+
+        $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"))
+        
+    }
+   
+
+); 
